593256557ef504f7db2dec3adde4967a977918ab
Daniel Butler <cactusfly@gmail.com>
2016-10-24 13:45:44 -0400

    added broken Scope

--- scripts/app.js
+++ scripts/app.js
@@ -3,4 +3,10 @@ angular.module("todoListApp", [])
   $scope.helloWorld = function() {
     console.log("Hello there! This is the helloWorld controller function, in the mainCtrl!");
   };
+})
+
+.controller('coolCtrl', function($scope) {
+  $scope.whoAmI = function() {
+    console.log("hellow there, this is the coolCtrl function!");
+  }
 });
