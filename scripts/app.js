5be0dc2a246bc97d8a04abed8c7fd5a1e3a46c69
Daniel Butler <cactusfly@gmail.com>
2016-10-25 19:45:18 -0400

    added deleteTodo and saveTodo functions

--- scripts/app.js
+++ scripts/app.js
@@ -6,11 +6,20 @@ angular.module("todoListApp", [])
   };
 
   $scope.helloWorld = dataService.helloWorld;
+
   dataService.getTodos(function(response) {
     console.log(response.data);
     $scope.todos = response.data;
   });
 
+  $scope.deleteTodo = function(todo, $index) {
+    dataService.deleteTodo(todo);
+    $scope.todos.splice($index, 1);
+  };
+
+  $scope.saveTodo = function(todo) {
+    dataService.saveTodo(todo);
+  };
 })
 .service('dataService', function($http) {
   this.helloConsole = function() {
@@ -20,5 +29,15 @@ angular.module("todoListApp", [])
   this.getTodos = function(callback) {
     $http.get('mock/todos.json')
     .then(callback)
-  }
+  };
+
+  this.deleteTodo = function(todo) {
+    console.log("The " + todo.name + " has been deleted!");
+    // other logic
+  };
+
+  this.saveTodo = function(todo) {
+    console.log("The " + todo.name + " todo has been saved!");
+    // other logic
+  };
 });
